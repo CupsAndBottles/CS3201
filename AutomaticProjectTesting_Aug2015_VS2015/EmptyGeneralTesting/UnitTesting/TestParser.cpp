86e005a9e7963f0d2d5588a706cbbd5bc1929c3b
Su Gi Chandran <sollercoaster@hotmail.com>
2015-09-12 13:13:16 +0800

    Added parser unit tests

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/TestParser.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/TestParser.cpp
@@ -1,7 +1,11 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "parser.h"
-#include "parser.h"
+#include "SIMPLEParser.h"
+#include "SIMPLERules.h"
+
+#include <string>
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -10,8 +14,28 @@ namespace UnitTesting
 	TEST_CLASS(TestAST)
 	{
 	public:
-		TEST_METHOD(TestMethod1) {
+		TEST_METHOD(testGoodAssign) {
+			vector<string> stmt;
+			stmt.push_back("x");
+			stmt.push_back("=");
+			stmt.push_back("y");
+			stmt.push_back("+");
+			stmt.push_back("1");
+			stmt.push_back(";");
 
+			simpleParser *p = new simpleParser(stmt);
+			Assert::AreEqual(true, (*p).parseAssign());
+		}
+
+		TEST_METHOD(testEmptyProcedure) {
+			vector<string> proc;
+			proc.push_back("procedure");
+			proc.push_back("Empty");
+			proc.push_back("{");
+			proc.push_back("}");
+
+			simpleParser *p = new simpleParser(proc);
+			Assert::AreEqual(false, (*p).parseProcedure());
 		}
 	};
 }
