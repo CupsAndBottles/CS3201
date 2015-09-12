c229dd17b17ee2f1d2aa9bb07f0ae9f3d514930f
Su Gi Chandran <sollercoaster@hotmail.com>
2015-09-12 22:21:31 +0800

    Added integration test

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/TestFrontEnd.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/TestFrontEnd.cpp
@@ -1,17 +1,35 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "Parser.h"
+#include "AbstractSyntaxTree.h"
+
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace IntegrationTesting
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(IntegrationTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(testParserASTIntegration)
 		{
-			// TODO: Your test code here
+			ofstream outputFile("program.txt");
+			outputFile << "procedure Proc {";
+			outputFile << "x = y + 1;";
+			outputFile << "}";
+			outputFile.close();
+
+			Parser *parse = new Parser();
+			vector<string> parsedProgram = (*parse).parseSimpleProgram("program.txt");
+			Assert::AreNotEqual(0, (int) parsedProgram.size());
+
+			AbstractSyntaxTree *AST = new AbstractSyntaxTree();
+			AST->buildAbstractSyntaxTree(parsedProgram);
+			Tnode *root = AST->getRoot();
 		}
 
 	};
