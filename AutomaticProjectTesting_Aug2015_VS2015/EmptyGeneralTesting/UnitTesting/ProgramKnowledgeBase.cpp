85e745a29a0aed785f168219c63e67bdf8b28843
Joey C <joeycyq@gmail.com>
2015-09-13 13:09:14 +0800

    Add getWhile, getProc, getVar unit tests for PKB

---
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/ProgramKnowledgeBase.cpp
@@ -0,0 +1,77 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Parser.h"
+#include "AbstractSyntaxTree.h"
+#include "ProgramKnowledgeBase.h"
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(TestPKB)
+	{
+	public:
+		TEST_METHOD(testPKBGetProcedures) {
+			ofstream outputFile("program.txt");
+			outputFile << "procedure Proc {";
+			outputFile << "x = y + 1;";
+			outputFile << "}";
+			outputFile.close();
+
+			Parser *parse = new Parser();
+			vector<string> parsedProgram = (*parse).parseSimpleProgram("program.txt");
+			AbstractSyntaxTree *AST = new AbstractSyntaxTree();
+			AST->buildAbstractSyntaxTree(parsedProgram);
+			Tnode *root = AST->getRoot();
+			ProgramKnowledgeBase* pkb = &ProgramKnowledgeBase(AST);
+			vector<string> procedures = pkb->getStringsOfType(Tnode::PROCEDURE);
+			Assert::AreEqual(string("Proc"), procedures[0]);
+		}
+
+		TEST_METHOD(testPKBGetVariables) {
+			ofstream outputFile("program.txt");
+			outputFile << "procedure Proc {";
+			outputFile << "x = y + 1;";
+			outputFile << "}";
+			outputFile.close();
+
+			Parser *parse = new Parser();
+			vector<string> parsedProgram = (*parse).parseSimpleProgram("program.txt");
+			AbstractSyntaxTree *AST = new AbstractSyntaxTree();
+			AST->buildAbstractSyntaxTree(parsedProgram);
+			Tnode *root = AST->getRoot();
+			ProgramKnowledgeBase* pkb = &ProgramKnowledgeBase(AST);
+
+			vector<string> variables = pkb->getStringsOfType(Tnode::VARIABLE);
+			Assert::AreEqual(2, int(variables.size()));
+			Assert::AreEqual(string("x"), variables[0]);
+			Assert::AreEqual(string("y"), variables[1]);
+		}
+
+		TEST_METHOD(testPKBGetWhiles) {
+			ofstream outputFile("program.txt");
+			outputFile << "procedure Proc {";
+			outputFile << "x = 1;";
+			outputFile << "while x {";
+			outputFile << "x = x - 1;";
+			outputFile << "}";
+			outputFile << "}";
+			outputFile.close();
+
+			Parser *parse = new Parser();
+			vector<string> parsedProgram = (*parse).parseSimpleProgram("program.txt");
+			AbstractSyntaxTree *AST = new AbstractSyntaxTree();
+			AST->buildAbstractSyntaxTree(parsedProgram);
+			Tnode *root = AST->getRoot();
+			ProgramKnowledgeBase* pkb = &ProgramKnowledgeBase(AST);
+
+			vector<int> whiles = pkb->getStatementsOfType(Tnode::STMT_WHILE);
+			Assert::AreEqual(1, int(whiles.size()));
+			Assert::AreEqual(2, whiles[0]);
+		}
+	};
+}
