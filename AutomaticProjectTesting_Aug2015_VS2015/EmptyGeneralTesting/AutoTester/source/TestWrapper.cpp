ad9eb58a4935e45ae5be75b858e81a42c4adb09a
Duan Xu Zhou <dxz1020@hotmail.com>
2015-09-29 00:05:50 +0800

    block errors in testwrapper

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/AutoTester/source/TestWrapper.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/AutoTester/source/TestWrapper.cpp
@@ -44,7 +44,7 @@ void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
   // ...code to evaluate query...
 
 	QueryEvaluator qe = QueryEvaluator(*pkbPointer);	
-	results = qe.getResults(query);
+	//results = qe.getResults(query);
   // store the answers to the query in the results list (it is initially empty)
   // each result must be a string.
 }
