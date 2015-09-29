ba4b212cfb05e87ec1d87e6c886b90f85b8bf820
wclim <wclim@hotmail.sg>
2015-09-29 21:15:27 +0800

    added a script to the 3 simple tests cases we have

---
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/testAll.bat
@@ -0,0 +1,5 @@
+@echo off
+AutoTester Simple01-Source.txt Simple01-Queries.txt out.xml
+AutoTester Simple02-Source.txt Simple02-Queries.txt out.xml
+AutoTester Simple03-Source.txt Simple03-Queries.txt out.xml
+pause
