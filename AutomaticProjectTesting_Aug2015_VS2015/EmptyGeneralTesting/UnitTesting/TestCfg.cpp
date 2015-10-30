3c5f060fcc2cd75c66be1999c593faf427007b39
Tricia Tan <tricia.tzy@gmail.com>
2015-10-31 01:18:10 +0800

    unittest cfg

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/TestCfg.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/TestCfg.cpp
@@ -15,7 +15,7 @@ namespace UnitTesting
 	TEST_CLASS(TestCFG)
 	{
 	public:
-		TEST_METHOD(TestNodeCreation) {
+		TEST_METHOD(TestGnodeCreation) {
 			Gnode *testNode;
 			testNode = Gnode::createGnode(5);
 
@@ -24,6 +24,18 @@ namespace UnitTesting
 			Assert::IsNull(testNode->getPrevLeft());
 			Assert::IsNull(testNode->getPrevRight());
 			Assert::AreEqual(4, (int)testNode->getType()); // 4 = NOTYPE;
+			Assert::AreEqual(5, testNode->getValue());
+		}
+
+		TEST_METHOD(TestGnodeTypeCreation) {
+			Gnode *testNode;
+			testNode = Gnode::createGnode(Gnode::STMT_WHILE, 5);
+
+			Assert::IsNull(testNode->getRight());
+			Assert::IsNull(testNode->getLeft());
+			Assert::IsNull(testNode->getPrevLeft());
+			Assert::IsNull(testNode->getPrevRight());
+			Assert::AreEqual(0, (int)testNode->getType()); // 0 = STMT_WHILE;
 			Assert::AreEqual(5, testNode->getValue());
 		}
 
@@ -54,10 +66,21 @@ namespace UnitTesting
 			db->buildDatabase(parsedProgram);
 			vector<Gnode*> vectornodes = db->createControlFlowGraphNodes();
 
+			Assert::AreEqual(1, vectornodes.at(1)->getValue());
+			Assert::AreEqual(2, vectornodes.at(2)->getValue());
+			Assert::AreEqual(3, vectornodes.at(3)->getValue());
+			Assert::AreEqual(4, vectornodes.at(4)->getValue());
+			Assert::AreEqual(5, vectornodes.at(5)->getValue());
+			Assert::AreEqual(6, vectornodes.at(6)->getValue());
+			Assert::AreEqual(7, vectornodes.at(7)->getValue());
+			Assert::AreEqual(8, vectornodes.at(8)->getValue());
+			Assert::AreEqual(9, vectornodes.at(9)->getValue());
+			Assert::AreEqual(10, vectornodes.at(10)->getValue());
+			Assert::AreEqual(11, vectornodes.at(11)->getValue());
 			Assert::AreEqual(12, vectornodes.at(12)->getValue());
 		}
 
-		TEST_METHOD(testBuildCfg) {
+		TEST_METHOD(testLinkNoNesting) {
 			string fileName = "source.txt";
 			ofstream outputFile(fileName, ofstream::trunc);
 			outputFile << "procedure Proc {";
@@ -84,7 +107,123 @@ namespace UnitTesting
 			db->buildDatabase(parsedProgram);
 			Gnode *cfgRoot = db->buildControlFlowGraph();
 
-			Assert::AreEqual(1, cfgRoot->getValue());
+			Assert::AreEqual(2, cfgRoot->getNext().at(0)->getValue());
+		}
+
+		TEST_METHOD(testLinkIfElseNestedInWhile) {
+			string fileName = "source.txt";
+			ofstream outputFile(fileName, ofstream::trunc);
+			outputFile << "procedure Proc {";
+			outputFile << "x = 1;"; // 1
+			outputFile << "z = y;"; // 2
+			outputFile << "while a {"; // 3
+			outputFile << "a = a - 1;"; // 4
+			outputFile << "if x then {"; // 5
+			outputFile << "a = b;"; // 6
+			outputFile << "z = 1; }"; // 7
+			outputFile << "else {";
+			outputFile << "c = a;"; // 8
+			outputFile << "x = 1; }"; //9
+			outputFile << "a = a - 2; }";//10
+			outputFile << "y = z;"; // 11
+			outputFile << "}";
+			outputFile.close();
+
+			Parser *parse = new Parser();
+			vector<string> parsedProgram = parse->parseSimpleProgram(fileName);
+			remove(fileName.c_str());
+			Database* db = new Database();
+			db->buildDatabase(parsedProgram);
+			Gnode *cfgRoot = db->buildControlFlowGraph();
+
+			Assert::AreEqual(2, cfgRoot->getNext().at(0)->getValue());
+		}
+
+
+		TEST_METHOD(testLinkWhileNestedInIf) {
+			string fileName = "source.txt";
+			ofstream outputFile(fileName, ofstream::trunc);
+			outputFile << "procedure Proc {";
+			outputFile << "x = 1;"; // 1
+			outputFile << "z = y;"; // 2
+			outputFile << "if x then {"; // 3
+			outputFile << "a = b;"; // 4
+			outputFile << "z = 1;"; // 5
+			outputFile << "while a {"; // 6
+			outputFile << "a = a - 1; }}"; // 7
+			outputFile << "else {"; 
+			outputFile << "c = a;"; // 8
+			outputFile << "x = 1; }"; // 9
+			outputFile << "a = a - 2;"; // 10
+			outputFile << "}";
+			outputFile.close();
+
+			Parser *parse = new Parser();
+			vector<string> parsedProgram = parse->parseSimpleProgram(fileName);
+			remove(fileName.c_str());
+			Database* db = new Database();
+			db->buildDatabase(parsedProgram);
+			Gnode *cfgRoot = db->buildControlFlowGraph();
+
+			Assert::AreEqual(2, cfgRoot->getNext().at(0)->getValue());
+		}
+
+		TEST_METHOD(testLinkWhileNestedInElse) {
+			string fileName = "source.txt";
+			ofstream outputFile(fileName, ofstream::trunc);
+			outputFile << "procedure Proc {";
+			outputFile << "x = 1;"; // 1
+			outputFile << "z = y;"; // 2
+			outputFile << "if x then {"; // 3
+			outputFile << "a = b;"; // 4
+			outputFile << "z = 1;}"; // 5
+			outputFile << "else {"; 
+			outputFile << "c = a;"; // 6
+			outputFile << "x = 1;"; // 7
+			outputFile << "while a {"; // 8
+			outputFile << "a = a - 1; }}"; // 9
+			outputFile << "a = a - 2;"; // 10
+			outputFile << "}";
+			outputFile.close();
+
+			Parser *parse = new Parser();
+			vector<string> parsedProgram = parse->parseSimpleProgram(fileName);
+			remove(fileName.c_str());
+			Database* db = new Database();
+			db->buildDatabase(parsedProgram);
+			Gnode *cfgRoot = db->buildControlFlowGraph();
+
+			Assert::AreEqual(2, cfgRoot->getNext().at(0)->getValue());
+		}
+
+		TEST_METHOD(testLinkWhileNestedInIfAndElse) {
+			string fileName = "source.txt";
+			ofstream outputFile(fileName, ofstream::trunc);
+			outputFile << "procedure Proc {";
+			outputFile << "x = 1;"; // 1
+			outputFile << "z = y;"; // 2
+			outputFile << "if x then {"; // 3
+			outputFile << "a = b;"; // 4
+			outputFile << "z = 1;"; // 5
+			outputFile << "while b {"; // 6
+			outputFile << "b = b - 1; }}"; // 7
+			outputFile << "else {"; 
+			outputFile << "c = a;"; // 9
+			outputFile << "x = 1;"; // 10
+			outputFile << "while a {"; // 11
+			outputFile << "a = a - 1; }}"; // 12
+			outputFile << "a = a - 2;"; // 13
+			outputFile << "}";
+			outputFile.close();
+
+			Parser *parse = new Parser();
+			vector<string> parsedProgram = parse->parseSimpleProgram(fileName);
+			remove(fileName.c_str());
+			Database* db = new Database();
+			db->buildDatabase(parsedProgram);
+			Gnode *cfgRoot = db->buildControlFlowGraph();
+
+			Assert::AreEqual(2, cfgRoot->getNext().at(0)->getValue());
 		}
 	};
 }
