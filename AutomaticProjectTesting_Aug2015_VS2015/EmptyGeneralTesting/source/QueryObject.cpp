f51494e6e434d5bb3580dfda441f56bd486b363e
Joey C <joeycyq@gmail.com>
2015-11-06 14:01:49 +0800

    Fix namespace/linker bug

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/source/QueryObject.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/source/QueryObject.cpp
@@ -1,22 +1,22 @@
 #include "QueryObject.h"
 
-const string RelationType_MODIFIES = "Modifies";
-const string RelationType_USES = "Uses";
-const string RelationType_PARENT = "Parent";
-const string RelationType_PARENTSTAR = "Parent*";
-const string RelationType_NEXT = "Next";
-const string RelationType_NEXTSTAR = "Next*";
-const string RelationType_FOLLOWS = "Follows";
-const string RelationType_FOLLOWSSTAR = "Follows*";
-const string RelationType_CALLS = "Calls";
-const string RelationType_CALLSSTAR = "Calls*";
-const string RelationType_AFFECTS = "Affects";
-const string RelationType_AFFECTSSTAR = "Affects*";
-const string RelationType_PATTERN_ASSIGN = "assign";
-const string RelationType_PATTERN_IF = "while";
-const string RelationType_PATTERN_WHILE = "if";
+const string QueryObject::RelationType_MODIFIES = "Modifies";
+const string QueryObject::RelationType_USES = "Uses";
+const string QueryObject::RelationType_PARENT = "Parent";
+const string QueryObject::RelationType_PARENTSTAR = "Parent*";
+const string QueryObject::RelationType_NEXT = "Next";
+const string QueryObject::RelationType_NEXTSTAR = "Next*";
+const string QueryObject::RelationType_FOLLOWS = "Follows";
+const string QueryObject::RelationType_FOLLOWSSTAR = "Follows*";
+const string QueryObject::RelationType_CALLS = "Calls";
+const string QueryObject::RelationType_CALLSSTAR = "Calls*";
+const string QueryObject::RelationType_AFFECTS = "Affects";
+const string QueryObject::RelationType_AFFECTSSTAR = "Affects*";
+const string QueryObject::RelationType_PATTERN_ASSIGN = "assign";
+const string QueryObject::RelationType_PATTERN_IF = "while";
+const string QueryObject::RelationType_PATTERN_WHILE = "if";
 
-const string WILDCARD = "_";
+const string QueryObject::WILDCARD = "_";
 
 QueryObject::QueryObject(string s1, string s2, string s3) {
 	arg1 = s1;
