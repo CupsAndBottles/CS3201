3150365b3fb210f5fe5ad9926b5c313917c5cb4d
Joey C <joeycyq@gmail.com>
2015-11-06 09:53:22 +0800

    Change vector of children nodes to unordered_set of pointers to children nodes; add isOnlyChild and isRoot; add pointer to parent; add destroy method

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Source/QueryNode.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Source/QueryNode.cpp
@@ -15,15 +15,40 @@ QueryNode::QueryNode(string syn, string ans) {
 	synonym = syn;
 }
 
-void QueryNode::addChild(QueryNode node) {
-	children.push_back(node);
+void QueryNode::setParent(QueryNode* node){
+	this->parent = node;
+}
+
+void QueryNode::destroy() {
+	if (!this->isRoot()) {
+		if (this->isOnlyChild()) {
+			parent->destroy();
+		} else {
+			parent->removeChild(this);
+		}
+	}
+
+	for (QueryNode* child : children) {
+		child->destroy();
+	}
+}
+
+void QueryNode::removeChild(QueryNode* node) {
+	children.erase(node);
+}
+
+void QueryNode::addChild(QueryNode* node) {
+	children.insert(node);
+	node->setParent(this);
 }
 
 void QueryNode::addChild(string syn, string ans) {
-	children.push_back(QueryNode(syn,ans));
+	QueryNode newNode = QueryNode(syn, ans);
+	newNode.setParent(this);
+	children.insert(&newNode);
 }
 
-vector<QueryNode> QueryNode::getChildren() {
+unordered_set<QueryNode*> QueryNode::getChildren() {
 	return children;
 }
 
@@ -33,4 +58,16 @@ string QueryNode::getAns() {
 
 string QueryNode::getSyn() {
 	return synonym;
+}
+
+bool QueryNode::isOnlyChild()
+{
+	if (!this->isRoot()) {
+		return (int)parent->getChildren().size() == 1;
+	}
+	return false;
+}
+
+bool QueryNode::isRoot() {
+	return parent == NULL;
 }
--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Source/QueryNode.h
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Source/QueryNode.h
@@ -1,11 +1,11 @@
 #ifndef QueryNode_HEADER
 #define QueryNode_HEADER
-#include <vector>
+#include <unordered_set>
 #include <string>
 
 using namespace std;
 
-class QueryNode 
+class QueryNode
 {
 
 public:
@@ -13,14 +13,21 @@ public:
 	~QueryNode();
 	QueryNode(string synonym, string ans);
 
-	void addChild(QueryNode node);
+	void QueryNode::setParent(QueryNode* node);
+	void destroy();
+	void removeChild(QueryNode * node);
+	void addChild(QueryNode* node);
 	void addChild(string syn, string ans);
-	vector<QueryNode> getChildren();
+	unordered_set<QueryNode*> getChildren();
 	string getAns();
 	string getSyn();
 
+	bool isOnlyChild();
+	bool isRoot();
+
 private:
-	vector<QueryNode> children;
+	unordered_set<QueryNode*> children;
+	QueryNode* parent;
 	string answer;
 	string synonym;
 };
