a51eb49924c85ef4102ecc95d18b41748123cb09
Joey C <joeycyq@gmail.com>
2015-11-09 11:18:09 +0800

    Unit test for QE select (excluding containers)

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/TestQueryEvaluator.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/TestQueryEvaluator.cpp
@@ -17,12 +17,16 @@ namespace UnitTesting
 	TEST_CLASS(TestQueryEvaluator)
 	{
 	public:
-		TEST_METHOD(testSelect) {
-			string fileName = "programGetProcedures.txt";
+		TEST_METHOD(testSelectWithoutContainers) {
+			string fileName = "programSelectWithoutContainers.txt";
 			ofstream outputFile(fileName, ofstream::trunc);
 			outputFile << "procedure Proc {";
 			outputFile << "x = y + 1;";
-			outputFile << "y = z + 1;";
+			outputFile << "y = z + 2;";
+			outputFile << "}";
+			outputFile << "procedure Other {";
+			outputFile << "a = b + 3;";
+			outputFile << "b = c + 1;";
 			outputFile << "}";
 			outputFile.close();
 
@@ -32,23 +36,48 @@ namespace UnitTesting
 			Database* db = new Database();
 			db->buildDatabase(parsedProgram);
 			ProgramKnowledgeBase pkb = ProgramKnowledgeBase(db);
-
 			QueryEvaluator qe = QueryEvaluator(&pkb);
-			list<string> output=qe.getResults("variable v; Select v");
-			Assert::AreEqual(3, int(output.size()));
-			string test;
-			for (auto v: output) {
-				test += v;
-			}
-			Assert::AreEqual(string("xyz"), test);
 			
-			output = qe.getResults("assign a; Select a");
-			Assert::AreEqual(2, int(output.size()));
-			test="";
-			for (auto v : output) {
-				test += v;
-			}
-			Assert::AreEqual(string("12"), test);
+			list<string> variables = qe.getResults("variable v; Select v");
+			Assert::AreEqual(6, int(variables.size()));
+			Assert::IsTrue(find(variables.begin(), variables.end(), string("x")) != variables.end());
+			Assert::IsTrue(find(variables.begin(), variables.end(), string("y")) != variables.end());
+			Assert::IsTrue(find(variables.begin(), variables.end(), string("z")) != variables.end());
+			Assert::IsTrue(find(variables.begin(), variables.end(), string("a")) != variables.end());
+			Assert::IsTrue(find(variables.begin(), variables.end(), string("b")) != variables.end());
+			Assert::IsTrue(find(variables.begin(), variables.end(), string("c")) != variables.end());
+			
+			list<string> procedures = qe.getResults("procedure p; Select p");
+			Assert::AreEqual(2, int(procedures.size()));
+			Assert::IsTrue(find(procedures.begin(), procedures.end(), string("Proc")) != procedures.end());
+			Assert::IsTrue(find(procedures.begin(), procedures.end(), string("Other")) != procedures.end());
+
+			list<string> constants = qe.getResults("constant c; Select c");
+			Assert::AreEqual(3, int(constants.size()));
+			Assert::IsTrue(find(constants.begin(), constants.end(), string("1")) != constants.end());
+			Assert::IsTrue(find(constants.begin(), constants.end(), string("2")) != constants.end());
+			Assert::IsTrue(find(constants.begin(), constants.end(), string("3")) != constants.end());
+
+			list<string> assigns = qe.getResults("assign a; Select a");
+			Assert::AreEqual(4, int(assigns.size()));
+			Assert::IsTrue(find(assigns.begin(), assigns.end(), string("1")) != assigns.end());
+			Assert::IsTrue(find(assigns.begin(), assigns.end(), string("2")) != assigns.end());
+			Assert::IsTrue(find(assigns.begin(), assigns.end(), string("3")) != assigns.end());
+			Assert::IsTrue(find(assigns.begin(), assigns.end(), string("4")) != assigns.end());
+
+			list<string> statements = qe.getResults("stmt s; Select s");
+			Assert::AreEqual(4, int(statements.size()));
+			Assert::IsTrue(find(statements.begin(), statements.end(), string("1")) != statements.end());
+			Assert::IsTrue(find(statements.begin(), statements.end(), string("2")) != statements.end());
+			Assert::IsTrue(find(statements.begin(), statements.end(), string("3")) != statements.end());
+			Assert::IsTrue(find(statements.begin(), statements.end(), string("4")) != statements.end());
+
+			list<string> progLines = qe.getResults("prog_line n; Select n");
+			Assert::AreEqual(4, int(progLines.size()));
+			Assert::IsTrue(find(progLines.begin(), progLines.end(), string("1")) != progLines.end());
+			Assert::IsTrue(find(progLines.begin(), progLines.end(), string("2")) != progLines.end());
+			Assert::IsTrue(find(progLines.begin(), progLines.end(), string("3")) != progLines.end());
+			Assert::IsTrue(find(progLines.begin(), progLines.end(), string("4")) != progLines.end());
 		}
 
 		TEST_METHOD(testCalls) {
