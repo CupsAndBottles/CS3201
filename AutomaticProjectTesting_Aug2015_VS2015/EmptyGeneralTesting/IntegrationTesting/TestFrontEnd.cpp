e08a3b67ebe8b986762f72d36490d9f9cf95734e
Tricia Tan <tricia.tzy@gmail.com>
2015-11-12 01:56:10 +0800

    added system testing w source 5

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/TestFrontEnd.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/TestFrontEnd.cpp
@@ -44,6 +44,40 @@ namespace IntegrationTesting
 			db->buildDatabase(parsedProgram);
 		}
 
+		TEST_METHOD(testParserDatabaseIntegrationWithSource5) {
+			string fileName = "testParserDatabaseIntegrationWithSource5.txt";
+			ofstream outputFile(fileName, ofstream::trunc);
+			outputFile << "procedure brown {";
+			outputFile << "while x {";
+			outputFile << "x = x + 1;";
+			outputFile << "if x then {";
+			outputFile << "if y then {";
+			outputFile << "if z then {";
+			outputFile << "a = x + y - z;}";
+			outputFile << "else {";
+			outputFile << "a = x + y + z;}}";
+			outputFile << "else {";
+			outputFile << "x = 0;}}";
+			outputFile << "else {";
+			outputFile << "x = 8 * z;}";
+			outputFile << "while y {";
+			outputFile << "if y then {";
+			outputFile << "y = y * z;}";
+			outputFile << "else {";
+			outputFile << "y = x * z;}";
+			outputFile << "}";
+			outputFile << "}";
+			outputFile << "}";
+			outputFile.close();
+			
+			Parser *parse = new Parser();
+			vector<string> parsedProgram = (*parse).parseSimpleProgram(fileName);
+			Assert::AreNotEqual(0, (int)parsedProgram.size());
+
+			Database* db = new Database();
+			db->buildDatabase(parsedProgram);
+		}
+
 		TEST_METHOD(testParserDatabasePKBIntegration) {
 			string fileName = "testParserDatabasePKBIntegration.txt";
 			ofstream outputFile(fileName, ofstream::trunc);
